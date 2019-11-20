e7d20112efd8d0080982ea320fda9d61ed7f1c8c
ngiersz <ngiersz@gmail.com>
2019-11-20 20:27:44 +0100

    Cleanup

--- project1/frequencies_modification.erl
+++ project1/frequencies_modification.erl
@@ -4,17 +4,13 @@
 %% These are the start functions used to create and
 %% initialize the server.
 start() ->
-    % List = [1,2,3,4],
-    % io:format("~p~n", [List]),
     register(frequency_server, spawn(frequencies_modification, init, [])).
 
 init() ->
     Frequencies = {get_frequencies(), []},
     loop(Frequencies).
 
-% Hard Coded -> {Pid, Frequency}
 get_frequencies() -> [10,11].
-% get_frequencies() -> [{0, 10},{0, 11}].
 
 % Phone
 start_phone() ->
@@ -26,39 +22,28 @@ start_phone() ->
 get_pid_value(Pid) ->
     PidStr = pid_to_list(Pid),
     PidStr1 = lists:sublist(PidStr, 2, length(PidStr)-2),
-    [N, P1, P2] = [list_to_integer(T) || T <- string:tokens(PidStr1,[$.])],
+    [_, P1, _] = [list_to_integer(T) || T <- string:tokens(PidStr1,[$.])],
     P1.
 
 init_phone() ->
     Frequencies = [],
     loop_phone(Frequencies).
 
-
-loop_phone(Frequencies) ->
+loop_phone(_) ->
     receive
         allocate ->
-            % io:format("~p~n", [Frequencies]),
-            % {NewFrequencies, Reply} = allocate(Frequencies, self()),
             {NewFrequencies, Reply} = call(self(), allocate),
-            % reply(self(), Reply),
-            % io:format("~p~n", [NewFrequencies]),
-            io:format("~p~n", [Reply]),
+            io:format("Allocated frequency: ~p~n", [Reply]),
             loop_phone(NewFrequencies);
-            % loop_phone(Frequencies);
         {deallocate, Freq} ->
             NewFrequencies = call(self(), {deallocate, Freq}),
-            % reply(Pid, ok),
             loop_phone(NewFrequencies);
         stop ->
-            reply(self(), ok)
+            ok
     end.
 
 % Client
 stop() -> call(self(), stop).
-
-% allocate() -> call(allocate).
-
-% deallocate(Freq) -> call({deallocate, Freq}).
 
 %% We hide all message passing and the message
 %% protocol in a functional interface.
@@ -72,16 +57,12 @@ call(Pid, Message) ->
 loop(Frequencies) ->
     receive
         {request, Pid, allocate} ->
-            % io:format("Before allocate: ~p~n", [Frequencies]),
             {NewFrequencies, Reply} = allocate(Frequencies, Pid),
             reply(Pid, Reply),
-            % io:format("After allocate: ~p~n", [NewFrequencies]),
             loop(NewFrequencies);
         {request, Pid , {deallocate, Freq}} ->
-            % io:format("Before deallocate: ~p~n", [Frequencies]),
             NewFrequencies = deallocate(Frequencies, Freq, Pid),
             reply(Pid, ok),
-            % io:format("After deallocate: ~p~n", [NewFrequencies]),
             loop(NewFrequencies);
         {request, Pid, stop} ->
             reply(Pid, ok)
@@ -106,19 +87,3 @@ deallocate({Free, Allocated}, Freq, Pid) ->
         true -> io:format("Frequency can not be allocated.~n"),
                 {Free, Allocated}
     end.
-
-%% The Internal Help Functions used to allocate and
-%% deallocate frequencies.
-% allocate({[], Allocated}, _Pid) ->
-%     {{[], Allocated}, {error, no_frequency}};
-% allocate({[Freq|Free], Allocated}, Pid) ->
-%     {{Free, [{Freq, Pid}|Allocated]}, {ok, Freq}}.
-
-% deallocate({Free, Allocated}, Freq, Pid) ->
-%     case {lists:member({Freq, Pid}, Allocated)} of 
-%         {true} -> true;
-%         {false} -> false
-%     end.
-%     % NewAllocated=lists:keydelete({Freq, Pid}, 1, Allocated),
-%     % {[Freq|Free], NewAllocated}.
-
